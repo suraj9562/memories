ea960e44bb0cab40eeca410d6f237bb725c6ff71
suraj9562 <wadikarsuraj02@gmail.com>
2022-07-10 23:55:04 +0530

    done with basic structure of routes

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,6 +1,47 @@
 const catchAsync = require("./../utils/catchAsync");
 
-exports.getPosts = catchAsync((req, res, next) => {
+// get all posts
+exports.getPosts = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {},
+  });
+});
+
+// delete all posts
+exports.deletePosts = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {},
+  });
+});
+
+// get particular post
+exports.getPost = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {},
+  });
+});
+
+// create new post
+exports.createPost = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {},
+  });
+});
+
+// update previously existing post
+exports.updatePost = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {},
+  });
+});
+
+// delete previously existing post
+exports.deletePost = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: "success",
     data: {},
--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { getPosts } = require("../controllers/postsController");
+const {
+  getPosts,
+  createPost,
+  getPost,
+  updatePost,
+  deletePost,
+  deletePosts,
+} = require("../controllers/postsController");
 
 const router = express.Router();
 
-router.route("/").get(getPosts);
+router.route("/").get(getPosts).post(createPost).delete(deletePosts);
 
+router.route("/:id").get(getPost).patch(updatePost).delete(deletePost);
 
 module.exports = router;
