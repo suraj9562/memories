b45d68e5b7f3f5f3c564e0fa4388f853ffd704ef
suraj9562 <wadikarsuraj02@gmail.com>
2022-07-11 12:14:56 +0530

    done with header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,7 +3,12 @@ import memories from "./../../assets/images/memories.png"
 function Header() {
   return (
     <div className={Styles.container}>
-      
+      <div className={Styles.title}>
+        Memories
+      </div>
+      <div className={Styles.image}>
+        <img src={memories} alt="logo" />
+      </div>
     </div>
   )
 }
